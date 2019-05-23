6583e6d7fc10b94591a96720d18b36dc76cee088
Andor Molnar <andor@apache.org>
2019-05-23 20:41:27 +0200

    CDPD-39584: ZOOKEEPER-3263 JAVA9/11 Warnings: Illegal reflective access in zookeeper's kerberosUtil (3.5)
    Fixes warning messages of JDK 9/11 by upgrading libraries and refactoring `KerberosUtils` based on experiences of [HADOOP-10848](https://issues.apache.org/jira/browse/HADOOP-10848)
    
    Reviewers please run unit tests with various JDK versions including 9 and 11, because CI only runs on JDK 8. Thanks.
    
    Target branches are master and 3.5
    
    Author: Andor Molnar <andorapache.org>
    
    Reviewers: Enrico Olivelli <eolivelliapache.org>
    
    Closes #950 from anmolnar/ZOOKEEPER-3263
    
    Author: Andor Molnar <andor@apache.org>
    
    Reviewers: eolivelli@apache.org
    
    Closes #954 from anmolnar/ZOOKEEPER-3263_35
    
    (cherry picked from commit 02dd927a75365ecba474f010a1f3c92bc62bb623)
    
    Change-Id: I87d7fc31d004fe70109fd1d8d6649d4557141135

--- zookeeper-server/src/test/java/org/apache/zookeeper/server/quorum/LeaderBeanTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/quorum/LeaderBeanTest.java
@@ -46,8 +46,7 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
